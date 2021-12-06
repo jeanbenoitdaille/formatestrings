1d5eee3ad54acd8b2b13a274853cf727f4718673
jean-benoit daille <68898672+jeanbenoitdaille@users.noreply.github.com>
2021-12-06 13:07:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const prenom = "Patrick";
+const age = "53";
+const moneyAccount01 = "10293";
+const moneyAccount02 = "392011";
+ 
+const presentation = `Bonjour, je m'appelle ${prenom} et j'ai ${age} ans. J'ai actuellement ${Number(moneyAccount01) + Number(moneyAccount02)}$ dans mes comptes en banque.
